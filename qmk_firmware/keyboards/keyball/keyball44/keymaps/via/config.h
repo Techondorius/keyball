2a6cebdc54fae123925aa67b023c877133ea3312
kyf <62565667+Techondorius@users.noreply.github.com>
2026-03-12 18:39:50 +0900

    fix: TAPPING_TERMを180msに変更
    150msでは短すぎる可能性があるため、デフォルト200msからの
    微調整として180msに変更。

--- qmk_firmware/keyboards/keyball/keyball44/keymaps/via/config.h
+++ qmk_firmware/keyboards/keyball/keyball44/keymaps/via/config.h
@@ -34,7 +34,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #endif
 
 #define TAP_CODE_DELAY 5
-#define TAPPING_TERM 150
+#define TAPPING_TERM 180
 
 #define DYNAMIC_KEYMAP_LAYER_COUNT 5
 
